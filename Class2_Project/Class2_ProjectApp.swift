ccdac61485be2de2e739c2012d9ef6cb3734afef
esquivel <esquivel>
2023-12-05 16:51:32 -0600

    Initial Commit

---
+++ Class2_Project/Class2_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Class2_ProjectApp.swift
+//  Class2_Project
+//
+//  Created by Gdaalumno on 05/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Class2_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Class2_Project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Class2_Project
+//
+//  Created by Gdaalumno on 05/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
